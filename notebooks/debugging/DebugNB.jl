604ff875ca78e233eaf3ba843b111c84dc90d42c
Neel Smith <nsmith@Technites.local>
2021-02-27 06:41:50 -0500

    Add module for developing notebook code more easily

---
+++ notebooks/debugging/DebugNB.jl
@@ -0,0 +1,75 @@
+module DebugNB
+
+using CitableText
+using CitableObject
+using CitableImage
+using CitableTeiReaders
+using CSV
+using DataFrames
+using EditionBuilders
+using EditorsRepo
+using HTTP
+using Lycian
+using Markdown
+using Orthography
+using PolytonicGreek
+
+export editorsrepo, catalogedtexts, catalogedurns
+export diplpassages, diplnode
+
+
+
+
+
+function editorsrepo() 
+    EditingRepository( dirname(pwd()), "editions", "dse", "config")
+end
+
+function catalogedtexts(repo::EditingRepository)
+	allcataloged = fromfile(CatalogedText, repo.root * "/" * "config" * "/catalog.cex")
+	filter(row -> row.online, allcataloged)
+end
+
+function catalogedurns(repo)
+    texts = catalogedtexts(repo)
+    texts[:, :urn]
+end
+
+
+# alldse = dse_df(editorsrepo())
+
+
+function diplpassages(editorsrepo)
+    urnlist = catalogedurns(editorsrepo)
+	try 
+		diplomaticarrays = map(u -> diplomaticnodes(editorsrepo, u), urnlist)
+		singlearray = reduce(vcat, diplomaticarrays)
+		filter(psg -> psg !== nothing, singlearray)
+	catch e
+		msg = "<div class='danger'><h1>🧨🧨 Markup error 🧨🧨</h1><p><b>$(e)</b></p></div>"
+		HTML(msg)
+	end
+end
+
+function diplnode(urn, diplomaticpassages)
+	generalized = dropversion(urn)
+	filtered = filter(cn -> urncontains(generalized, dropversion(cn.urn)), diplomaticpassages)
+	#filtered = filter(cn -> dropversion(cn.urn) == dropversion(urn), diplomaticpassages)
+    dropref = filter(cn -> ! isref(cn.urn), filtered)
+    
+	if length(dropref) > 0
+        content = collect(map(n -> n.text, dropref))
+        join(content, "\n")
+		#filtered[1].text
+	else 
+		""
+	end
+end
+
+function isref(urn::CtsUrn)::Bool
+    # True if last part of 
+    passageparts(urn)[end] == "ref"
+end
+
+
+end
