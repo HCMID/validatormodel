509fd7d7b01541806bfe9734f05d382d7c014b2a
Neel Smith <nsmith@Technites.local>
2021-02-27 06:43:33 -0500

    Add nb file for refeactored nb

---
+++ notebooks/midvalidator-0.10.0.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d859973a-78f0-11eb-05a4-13dba1f0cb9e
+"Hello MID"
+
+# ╔═╡ Cell order:
+# ╠═d859973a-78f0-11eb-05a4-13dba1f0cb9e
