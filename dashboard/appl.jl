468f36a39c60a30b904875da28c4051eb51a8733
Neel Smith <dnsmith.neel@gmail.com>
2022-02-07 11:57:30 -0500

    Define structure of page

--- dashboard/appl.jl
+++ dashboard/appl.jl
@@ -26,29 +26,78 @@ function iiifsvc()
 	"/project/homer/pyramidal/deepzoom")
 end
 
-#=
-function surfacemenu()
-	options = [""]
-	for s in surfaces(r)
-		push!(options, string(s))
-	end
-	options
-end
-=#
+#external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+#app = dash(external_stylesheets=external_stylesheets)
 app = dash()
 
 app.layout = html_div() do
     html_h1("MID validating notebook"),
-    html_div() do 
-        "Loaded repository: $(r isa EditingRepository)"
+  
+    html_button("Load/update data", id="load_button"),
+    html_div(children="No data loaded", id="datastate"),
+    
+    html_div(id="surfacemenu") do 
+        html_div(id="surfacepicker")
     end,
-    dcc_radioitems(
-        id = "surface",
-        options = [(label = "Select a surface", value = "")],
-        value = ""
-    )
+
+    html_div(id="thumb"),
+    html_div(id="dsecompleteness"),
+    html_div(id="dseaccuracy"),
+    html_div(id="orthography")
 
 end
 
+"Update surfaces menu and set user message about number of times data loaded."
+function updaterepodata(n_clicks)
+    msg = if isnothing(n_clicks)
+        dcc_markdown("*No data loaded yet*")
+    elseif n_clicks ==  1
+        dcc_markdown("Data loaded.")
+    else
+        dcc_markdown("""Data loaded **$(n_clicks)** times.""")
+    end
+#=
 
+    menupairs = [(label="", value="")]
+    for s in surfaces(r)
+		push!(menupairs, (label=string(s), value=string(s)))
+	end
+
+    menu = html_div(style = Dict("width" => "600px")) do
+        dcc_dropdown(
+            id = "surfacepicker",
+            options = menupairs,
+            value = ""
+        )
+    end
+
+    (msg, [dcc_markdown("## Choose a surface to verify"), menu] )
+=#
+    (msg, ["Message 2"])
+end
+
+function updatesurfacedata()
+end
+
+#=
+callback!(
+    updaterepodata,
+    app,
+    Output("datastate", "children"),
+    Output("surfacepicker", "children"),
+    Input("load_button", "n_clicks"),
+    prevent_initial_call=true
+)
+
+
+
+
+callback!(
+    app,
+    Output("thumb", "children"),
+    Input("surfacepicker", "value")
+) do newsurface
+    dcc_markdown("Woohoo! **$(newsurface)**")
+end
+=#
 run_server(app, "0.0.0.0", debug=true)
